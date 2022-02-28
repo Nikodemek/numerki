0982521ed4daea745a9c54986b9afcc47f4adc42
236559 Kirsz Nikodem <236559@edu.p.lodz.pl>
2022-02-28 14:30:49 +0100

    Najlepszy kodzik

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Func<double, double> exprLinear = x => 3 * x + 2.5;
+            double zeroLinear = FindZeroArgBisect(exprLinear, 2.0, 5.0, 0.000001);
+            Console.WriteLine($"Function y = 3x + 2.5 has a zero with x = {zeroLinear}, (y({zeroLinear}) = {exprLinear(zeroLinear):n7})");
+
+            Console.ReadKey();
+        }
+
+        private static double FindZeroArgBisect(Func<double, double> expr, double min, double max, double eps)
+        {
+            if (expr(min) * expr(max) > 0) throw new ArgumentException("Zły argument");
+
+            double upperBound = max;
+            double lowerBound = min;
+            double potentialZero = (upperBound + lowerBound) * 0.5;
+            double result;
+
+            do
+            {
+                result = expr(potentialZero);
+
+                if (result < 0)
+                {
+                    lowerBound = potentialZero;
+                }
+                else
+                {
+                    upperBound = potentialZero;
+                }
+                
+                potentialZero = (upperBound + lowerBound) * 0.5;
+            }
+            while (Math.Abs(result) > eps);
+
+            return potentialZero;
+        }
+    }
+}
