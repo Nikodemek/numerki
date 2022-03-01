9b54aa15345790cad511ffc79a3e1fa9b89d8044
236559 Kirsz Nikodem <236559@edu.p.lodz.pl>
2022-03-01 12:33:39 +0100

    Juz nie pamietam co

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -10,6 +10,9 @@ namespace Zadanie1
             double zeroLinear = FindZeroArgBisect(exprLinear, -2.0, 5.0, 0.000001);
             Console.WriteLine($"Function y = 3x + 2.5 has a zero with x = {zeroLinear}, (y({zeroLinear}) = {exprLinear(zeroLinear):n7})");
 
+            zeroLinear = FindZeroArgBisect(exprLinear, -2.0, 5.0, 9);
+            Console.WriteLine($"Function y = 3x + 2.5 has a zero with x = {zeroLinear}, (y({zeroLinear}) = {exprLinear(zeroLinear):n7})");
+
             Console.ReadKey();
         }
 
@@ -17,14 +20,14 @@ namespace Zadanie1
         {
             if (expr(min) * expr(max) > 0) throw new ArgumentException("Zły argument");
 
+            double prevPotentialZero = max;
             double upperBound = max;
             double lowerBound = min;
             double potentialZero = (upperBound + lowerBound) * 0.5;
-            double result = expr(potentialZero);
 
-            while (Math.Abs(result) > eps)
+            while (Math.Abs(prevPotentialZero - potentialZero) > eps)
             {
-                result = expr(potentialZero);
+                double result = expr(potentialZero);
 
                 if (result < 0)
                 {
@@ -34,7 +37,43 @@ namespace Zadanie1
                 {
                     upperBound = potentialZero;
                 }
-                
+
+                prevPotentialZero = potentialZero;
+                potentialZero = (upperBound + lowerBound) * 0.5;
+            }
+
+            return potentialZero;
+
+            /*                           ''        
+                '||''|, .|''|, `||''|,   ||  ('''' 
+                 ||  || ||..||  ||  ||   ||   `'') 
+                 ||..|' `|...  .||  ||. .||. `...' 
+                 ||                                
+                .|| 
+            */
+        }
+
+        private static double FindZeroArgBisect(Func<double, double> expr, double min, double max, int iterations)
+        {
+            if (expr(min) * expr(max) > 0) throw new ArgumentException("Zły argument");
+
+            double upperBound = max;
+            double lowerBound = min;
+            double potentialZero = (upperBound + lowerBound) * 0.5;
+
+            for (var i = 0; i < iterations; i++)
+            {
+                double result = expr(potentialZero);
+
+                if (result < 0)
+                {
+                    lowerBound = potentialZero;
+                }
+                else
+                {
+                    upperBound = potentialZero;
+                }
+
                 potentialZero = (upperBound + lowerBound) * 0.5;
             }
 
