ad5773fd335c412e1f3387a8c985b7e18d45edd4
Oskar Trela <boskichlopaczyna@gmail.com>
2022-03-02 18:49:25 +0100

    Incisal method implemented.

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,20 +1,36 @@
 ﻿using System;
 
+/*                           ''        
+    '||''|, .|''|, `||''|,   ||  ('''' 
+     ||  || ||..||  ||  ||   ||   `'') 
+     ||..|' `|...  .||  ||. .||. `...' 
+     ||                                
+    .|| 
+*/
+
 namespace Zadanie1
 {
     class Program
     {
         private static void Main(string[] args)
         {
-            Func<double, double> exprLinear = x => 3 * x + 2.5;
-            double zeroLinear = FindZeroArgBisect(exprLinear, -2.0, 5.0, 0.000001);
+            Func<double, double> exprLinear = x => x*x - 2;
+            Func<double, double> deriLinear = x => 2 * x;
+
+            double zeroLinear = FindZeroArgBisect(exprLinear, -1.0, 5.0, 0.000001);
+            Console.WriteLine($"Function y = 3x + 2.5 has a zero with x = {zeroLinear}, (y({zeroLinear}) = {exprLinear(zeroLinear):n7})");
+            zeroLinear = FindZeroArgIncisal(exprLinear, deriLinear, -1.0, 5.0, 0.01);
             Console.WriteLine($"Function y = 3x + 2.5 has a zero with x = {zeroLinear}, (y({zeroLinear}) = {exprLinear(zeroLinear):n7})");
 
-            zeroLinear = FindZeroArgBisect(exprLinear, -2.0, 5.0, 9);
+            zeroLinear = FindZeroArgBisect(exprLinear, -1.0, 5.0, 9);
+            Console.WriteLine($"Function y = 3x + 2.5 has a zero with x = {zeroLinear}, (y({zeroLinear}) = {exprLinear(zeroLinear):n7})");
+            zeroLinear = FindZeroArgIncisal(exprLinear, deriLinear, -1.0, 5.0, 1);
             Console.WriteLine($"Function y = 3x + 2.5 has a zero with x = {zeroLinear}, (y({zeroLinear}) = {exprLinear(zeroLinear):n7})");
 
             Console.ReadKey();
         }
+
+        // -------------------- Bisect --------------------
 
         private static double FindZeroArgBisect(Func<double, double> expr, double min, double max, double eps)
         {
@@ -43,25 +59,18 @@ namespace Zadanie1
             }
 
             return potentialZero;
-
-            /*                           ''        
-                '||''|, .|''|, `||''|,   ||  ('''' 
-                 ||  || ||..||  ||  ||   ||   `'') 
-                 ||..|' `|...  .||  ||. .||. `...' 
-                 ||                                
-                .|| 
-            */
         }
 
         private static double FindZeroArgBisect(Func<double, double> expr, double min, double max, int iterations)
         {
-            if (expr(min) * expr(max) > 0) throw new ArgumentException("Zły argument");
+            if (expr(min) * expr(max) > 0 
+                || iterations == 0) throw new ArgumentException("Zły argument");
 
             double upperBound = max;
             double lowerBound = min;
             double potentialZero = (upperBound + lowerBound) * 0.5;
 
-            for (var i = 0; i < iterations; i++)
+            for (var i = 0; i < iterations - 1; i++)
             {
                 double result = expr(potentialZero);
 
@@ -79,5 +88,41 @@ namespace Zadanie1
 
             return potentialZero;
         }
+
+        // -------------------- Incisal --------------------
+
+        private static double FindZeroArgIncisal(Func<double, double> expr, Func<double, double> deri, double min, double max, double eps)
+        {
+            if (expr(min) * expr(max) > 0) throw new ArgumentException("Zły argument");
+
+            double x = min;
+            double potentialZero = x - expr(x) / deri(x);
+
+            while (Math.Abs(potentialZero - x) > eps)
+            {
+                x = potentialZero;
+                potentialZero = x - expr(x) / deri(x);
+            }
+
+            return potentialZero;
+        }
+
+        private static double FindZeroArgIncisal(Func<double, double> expr, Func<double, double> deri, double min, double max, int iterations)
+        {
+            if (expr(min) * expr(max) > 0 
+                || iterations == 0) throw new ArgumentException("Zły argument");
+
+            double x = min;
+            double potentialZero = x - expr(x) / deri(x);
+
+            for (var i = 0; i < iterations - 1; i++)
+            {
+                x = potentialZero;
+                potentialZero = x - expr(x) / deri(x);
+            }
+
+            return potentialZero;
+        }
+
     }
 }
