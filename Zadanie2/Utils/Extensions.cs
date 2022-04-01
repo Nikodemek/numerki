990c34fdfc953b30522b6f8cb387550c708e5d15
236559 Kirsz Nikodem <236559@edu.p.lodz.pl>
2022-04-01 21:37:11 +0200

    Might have forgotten to attach sth

---
+++ Zadanie2/Utils/Extensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadanie2.Utils;
+
+public static class Extensions
+{
+    public static void Print(this double[,] matrix)
+    {
+        if (matrix is null) throw new ArgumentException("Matrix can not be null");
+
+        int xLength = matrix.GetLength(0);
+
+        if (xLength <= 0) throw new ArgumentException("Matrix can not be empty");
+
+        int yLength = matrix.GetLength(1);
+
+        string[,] values = new string[xLength, yLength];
+
+        int maxValLen = 0;
+        for (int i = 0; i < xLength; i++)
+        {
+            for (int j = 0; j < yLength; j++)
+            {
+                double value = matrix[i, j];
+                string valueToInsert = value.ToString();
+                int valueToInsertLen = valueToInsert.Length;
+
+                values[i, j] = valueToInsert;
+                if (valueToInsertLen > maxValLen) maxValLen = valueToInsertLen;
+            }
+        }
+
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < xLength; i++)
+        {
+            sb.Append('[');
+            for (int j = 0; j < yLength; j++)
+            {
+                string valueToInsert = values[i, j].PadLeft(maxValLen, ' ');
+                sb.Append(' ').Append(valueToInsert).Append(' ');
+            }
+            sb.Append("]\n");
+        }
+
+        Console.WriteLine(sb.ToString());
+    }
+}
