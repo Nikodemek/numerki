02eb48f40132627ca731e0e70ddbadeb41fa02ba
trela677 <236677@edu.p.lodz.pl>
2022-04-01 21:23:32 +0200

    Gauss elimination implemented

--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -1,9 +1,30 @@
 ﻿
+using Zadanie2;
+
 class Program {
   
     public static void Main()
     {
-        Console.WriteLine("Main Method");
+
+        var test = new double[,]
+        {
+            {3, 2, 1, -1},
+            {5, -1, 1, 2},
+            {1, -1, 1, 2},
+            {7, 8, 1, -7},
+        };
+
+        test = GaussSolution.Elimination(test);
+
+        for (int i = 0; i < test.GetLength(0); i++)
+        {
+            for (int j = 0; j < test.GetLength(1); j++)
+            {
+                Console.Write(test[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        
         Console.ReadKey();
     }
 }
