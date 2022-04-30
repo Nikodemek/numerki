4ee7684d364b2401fd9f02a929a5ed0b8b685a74
trela677 <236677@edu.p.lodz.pl>
2022-04-30 16:29:30 +0200

    Base project; FileReader implemented

---
+++ Zadanie3/Zadanie3/Program.cs
@@ -0,0 +1,18 @@
+﻿using Zadanie3.Dao;
+
+namespace Zadanie3;
+
+class Program {
+  
+    public static void Main()
+    {
+        var fileManager = new FileManager("test");
+        var sth = fileManager.Read();
+        foreach (var d in sth)
+        {
+            Console.WriteLine(d);
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ Zadanie3/Zadanie3/Utils/ArrayConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace Zadanie3.Utils;
+
+public class ArrayConverter
+{
+    public static double[,] ConvertToTwoDimensional(List<double[]> doublesList)
+    {
+        int rowLength = doublesList.Count;
+        int columnLength = doublesList[0].Length;
+
+        double[,] newArray = new double[rowLength, columnLength];
+
+        for (var i = 0; i < rowLength; i++)
+        {
+            for (int j = 0; j < columnLength; j++)
+            {
+                newArray[i, j] = doublesList[i][j];
+            }
+        }
+
+        return newArray;
+    }
+}
