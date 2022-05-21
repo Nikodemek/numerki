a3742feaf48343b93e068ddef3c995af505403bf
trela677 <236677@edu.p.lodz.pl>
2022-05-21 18:21:48 +0200

    Some Gauss

---
+++ Zadanie4/Model/GaussQuadrature.cs
@@ -0,0 +1,17 @@
+﻿namespace Zadanie4.Model;
+
+public class GaussQuadrature
+{
+    public double CalculateIntegral(Func<double, double> func, int knotsCount)
+    {
+        double result = 0;
+        double factor = Math.PI / knotsCount;
+        for (int i = 0; i < knotsCount; i++)
+        {
+            double x = Math.Cos((2 * i + 1) / 2 * knotsCount + 2) * Math.PI;
+            result += factor * x;
+        }
+
+        return result;
+    }
+}
