c7d59a195cf83bd3248f7ed27e2f248f42234e78
trela677 <236677@edu.p.lodz.pl>
2022-05-21 21:07:04 +0200

    Gauss is implemented with better formula now.

--- Zadanie4/Model/GaussQuadrature.cs
+++ Zadanie4/Model/GaussQuadrature.cs
@@ -5,10 +5,19 @@ public class GaussQuadrature
     public double CalculateIntegral(Func<double, double> func, int knotsCount)
     {
         double result = 0;
-        double factor = Math.PI / (knotsCount + 1);
+        
+        // to jest z wykładu i jest gorsze XD
+        /*double factor = Math.PI / (knotsCount + 1);
         for (int i = 0; i < knotsCount; i++)
         {
             double x = Math.Cos((2 * i + 1) * Math.PI / (2 * knotsCount + 2));
+            result += factor * func(x);
+        }*/
+        
+        double factor = Math.PI / knotsCount;
+        for (int i = 1; i <= knotsCount; i++)
+        {
+            double x = Math.Cos((2 * i - 1) * Math.PI / (2 * knotsCount));
             result += factor * func(x);
         }
 
